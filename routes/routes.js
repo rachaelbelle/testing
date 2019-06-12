267e1a634d789dd004a29f4c70c8262a872621b7
YvelStLaurent <rachaelbelle@gmail.com>
2019-06-12 18:14:50 -0400

    second day

--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,22 @@ var db = require('../models')
 // var router = express.Router()
 router.get("/", function(req, res){
   db.Chocolate.findAll({}).then(function(result){
-    res.json(result)
+    res.render('index', {peanut: result})
   })
 });
 
-router.post("/", function(req, res){
+router.get("/form", function(req, res){
+  res.render("form")
+});
+
+router.post("/api/post", function(req, res){
   db.Chocolate.create({
     "name" : req.body.name
   }).then(function(result){
-    res.json(result)
+    res.redirect("/")
   })
 });
+
+
 
 module.exports = router;
